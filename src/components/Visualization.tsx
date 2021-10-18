edb0885537fc09b9eaf7a938ee2ab442a46954b7
Bryan Mehall <bryanmehall@gmail.com>
2021-10-18 15:37:58 -0600

    init base configuration

---
+++ src/components/Visualization.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const Visualization = () => {
+    return <div>abc</div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { Visualization } from "./components/Visualization"
+
+ReactDOM.render(<Visualization></Visualization>, document.getElementById("app"))
