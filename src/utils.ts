a77865c29d278ebb1121a2b38e712c2a3c1e0a7f
Bryan Mehall <bryanmehall@gmail.com>
2021-10-18 20:47:15 -0600

    add reflection color util

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
-import { DirectionFlag } from "./types";
+import { DirectionFlag } from "./types"
 
 export const xScale = (direction: DirectionFlag): number => (
     direction == "left" ? -1 : 1
 )
+
+const reflectionColors = ["#00f", "#0f0", "#fe0", "#fa0", "#f00", "#f0f", "#a0f"]
+//cycle through colors then repeat
+export const refColor = (n: number): string => (reflectionColors[n % reflectionColors.length])
