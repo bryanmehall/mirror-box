050d860341e8777269df361ca3cf3e9ab1dfa1fb
Bryan Mehall <bryanmehall@gmail.com>
2021-10-19 23:10:39 -0600

    fix indentation in Draggable component

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -3,25 +3,30 @@ import * as React from "react"
 // adapted from Zibit Draggable component
 export const Draggable = (props) => {
     const mouseDown = (e) => {
-        if (props.hasOwnProperty('dragStart')){props.dragStart(e)}
+        if (props.hasOwnProperty('dragStart')) {
+            props.dragStart(e)
+        }
         document.addEventListener('mousemove', mouseMove)
         document.addEventListener('mouseup', mouseUp)
     }
     const mouseMove = (e) => {
-        if (props.hasOwnProperty('dragMove')){props.dragMove(e)}
+        if (props.hasOwnProperty('dragMove')) {
+            props.dragMove(e)
+        }
     }
     const mouseUp = (e) => {
-        if (props.hasOwnProperty('dragEnd')){props.dragEnd(e)}
+        if (props.hasOwnProperty('dragEnd')) {
+            props.dragEnd(e)
+        }
         document.removeEventListener('mousemove', mouseMove)
         document.removeEventListener('mouseup', mouseUp)
-
     }
     return(
         <g
             onMouseDown={mouseDown}
-            pointerEvents = 'all'
+            pointerEvents='all'
             cursor='pointer'
-            >
+        >
             {props.children}
         </g>
     )
