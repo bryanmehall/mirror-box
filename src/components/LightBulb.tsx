9e9fddb4f5fc3f5a7ff4b50a5e603a00bdb5c5ff
Bryan Mehall <bryanmehall@gmail.com>
2021-10-19 23:11:50 -0600

    fix spacing in LightBulb component

--- src/components/LightBulb.tsx
+++ src/components/LightBulb.tsx
@@ -9,17 +9,19 @@ type TargetProps = {
 }
 
 export const LightBulb = ({x, y, opacity, onClick, illuminated}: TargetProps): JSX.Element => {
-    return <g 
-        transform={`translate(${x-.3}, ${-y-.3})`}
-        onClick={onClick}
+    return (
+        <g 
+            transform={`translate(${x-.3}, ${y-.3})`}
+            onClick={onClick}
         >
-        <polygon 
-            fill={illuminated ? "yellow": "#777" }
-            opacity={illuminated ? 1 : opacity}
-            stroke="#fff"
-            strokeWidth="20"
-            transform={`scale(0.002)`}
-            points="300,150 225,280 75,280 0,150 75,20 225,20" 
-        />
-    </g>
+            <polygon 
+                fill={illuminated ? "yellow": "#777" }
+                opacity={illuminated ? 1 : opacity}
+                stroke="#fff"
+                strokeWidth="20"
+                transform="scale(0.002)"
+                points="300,150 225,280 75,280 0,150 75,20 225,20" 
+            />
+        </g>
+    )
 }
